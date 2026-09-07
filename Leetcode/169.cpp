7b4257a4f3b13de13873fca6b1a379dd83e489cc
Satvik Mishra <satvikmishra.shubh@gmail.com>
2026-09-07 10:27:30 +0530

    Implement Boyer-Moore algorithm for majority element

---
+++ Leetcode/169.cpp
@@ -0,0 +1,9 @@
+#Boyer_Moore
+int majorityElement(vector<int>& nums) {
+    int candidate = 0, count = 0;
+    for(int x : nums){
+        if(count == 0) candidate = x;
+        count += (x == candidate) ? 1 : -1;
+    }
+    return candidate;
+}
