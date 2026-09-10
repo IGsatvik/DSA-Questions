d5853c9b2112f170835c4af8b510628f3790c04d
Satvik Mishra <satvikmishra.shubh@gmail.com>
2026-09-10 16:37:50 +0530

    Delete Leetcode/First approach directory

--- Leetcode/First approach/169.cpp
+++
@@ -1,4 +0,0 @@
-int majorityElement(vector<int>& nums) {
-  sort(nums.begin(),nums.end());
-  return nums[nums.size()/2];
-}
--- Leetcode/First approach/215.cpp
+++
@@ -1,4 +0,0 @@
-int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()-k];
-    }
