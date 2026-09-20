74709112c648ec046823ed8e1bc2a8b266a4ceea
Satvik Mishra <satvikmishra.shubh@gmail.com>
2026-09-20 17:02:22 +0530

    Add numberOfSteps function to calculate steps to reduce a number to zero

---
+++ Leetcode/1342.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int c = 0;
+        while(num){
+            if(num & 1) num--;  
+            else num >>= 1;
+            c++;
+        }
+        return c;
+    }
+};
